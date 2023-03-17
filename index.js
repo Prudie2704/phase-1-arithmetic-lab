a63c011b827e914b0315022a729ed6461a74b797
prudence <prudence.jerono@student.moringaschool.com>
2023-03-17 12:54:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let random = Math.ceil(Math.random()) + 1;
+console.log(random)
+
+num3 = 24
+num4 = 5
+let mod = num3 % num4;
+console.log(mod)
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+console.log(max)
